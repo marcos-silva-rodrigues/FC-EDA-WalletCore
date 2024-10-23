06f206b59211f6402cb73bfc0d7c03d44f9bb5f3
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-10-23 13:23:59 +0000

    fix: add wiat groups

--- pkg/events/event_dispatcher.go
+++ pkg/events/event_dispatcher.go
@@ -1,6 +1,9 @@
 package events
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 var ErrHandlerAlreadyRegistered = errors.New("handler already registered")
 
@@ -14,13 +17,15 @@ func NewEventDispatcher() *EventDispatcher {
 	}
 }
 
-func (ed *EventDispatcher) Dispatch(event EventInterface) error {
-	if handlers, ok := ed.handlers[event.GetName()]; ok {
+func (ev *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := ev.handlers[event.GetName()]; ok {
+		wg := &sync.WaitGroup{}
 		for _, handler := range handlers {
-			handler.Handle(event)
+			wg.Add(1)
+			go handler.Handle(event, wg)
 		}
+		wg.Wait()
 	}
-
 	return nil
 }
 
